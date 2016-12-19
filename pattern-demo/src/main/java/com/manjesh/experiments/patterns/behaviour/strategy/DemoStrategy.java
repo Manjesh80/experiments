5f49bd400e7ff7675f396ee0d3c4de829be8222c
Gowda, Manjesh (mg153v) <mg153v@us.att.com>
2016-12-19 12:07:07 -0500

    Added Strategy pattern

--- pattern-demo/src/main/java/com/manjesh/experiments/patterns/behaviour/strategy/DemoStrategy.java
+++ pattern-demo/src/main/java/com/manjesh/experiments/patterns/behaviour/strategy/DemoStrategy.java
@@ -3,7 +3,7 @@ package com.manjesh.experiments.patterns.behaviour.strategy;
 /**
  * Author: mg153v (Manjesh Gowda). Creation Date: 12/19/2016.
  */
-public class StrategyDemo {
+public class DemoStrategy {
 
     public static void main(String[] args) {
         CompressionContext compressionContext = new CompressionContext();
