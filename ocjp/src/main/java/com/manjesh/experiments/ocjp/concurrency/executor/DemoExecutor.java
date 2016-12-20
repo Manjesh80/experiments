b844e563799e6fd01edc5a37e6ff4602fe909de7
Gowda, Manjesh (mg153v) <mg153v@us.att.com>
2016-12-20 17:35:38 -0500

    Added Strategy pattern

---
+++ ocjp/src/main/java/com/manjesh/experiments/ocjp/concurrency/executor/DemoExecutor.java
@@ -0,0 +1,20 @@
+package com.manjesh.experiments.ocjp.concurrency.executor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * Author: mg153v (Manjesh Gowda). Creation Date: 12/20/2016.
+ */
+public class DemoExecutor {
+
+    public static void main(String[] args) {
+
+    }
+
+    class SimpleRunnableTask implements Runnable {
+        public void run() {
+            System.out.println(" Simple runnable task ");
+        }
+    }
+}
