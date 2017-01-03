1aafdccb4465712ed4f7677f29c7faa4e1b668ba
Gowda, Manjesh (mg153v) <mg153v@us.att.com>
2017-01-03 14:26:57 -0500

    Added by Chain of Responsibility

---
+++ pattern-demo/src/main/java/com/manjesh/experiments/patterns/behaviour/chainofresponsibility/EmailHandler.java
@@ -0,0 +1,13 @@
+package com.manjesh.experiments.patterns.behaviour.chainofresponsibility;
+
+/**
+ * Author: mg153v (Manjesh Gowda). Creation Date: 12/27/2016.
+ */
+
+public interface EmailHandler {
+    //reference to the next handler in the chainpublic
+   /* void setNext(EmailHandler handler);
+
+    public void handleRequest(Email email);*/
+}
+
