08648a88da485d30154844c9db00ae13fb246026
Gowda, Manjesh (mg153v) <mg153v@us.att.com>
2017-01-06 17:19:32 -0500

    Added JSON format

--- rest-service/src/main/java/com/manjesh/rest/model/Event.java
+++ rest-service/src/main/java/com/manjesh/rest/model/Event.java
@@ -1,14 +1,18 @@
 package com.manjesh.rest.model;
 
 import java.time.OffsetDateTime;
+import javax.ws.rs.FormParam;
 
 /**
  * Author: mg153v (Manjesh Gowda). Creation Date: 1/5/2017.
  */
 public class Event {
 
+    //@FormParam("deviceID")
     private String deviceID;
+    //@FormParam("networkID")
     private String networkID;
+    //@FormParam("eventTime")
     private OffsetDateTime eventTime;
 
     public Event() {
--- rest-service/src/main/java/com/manjesh/rest/service/EventService.java
+++ rest-service/src/main/java/com/manjesh/rest/service/EventService.java
@@ -4,9 +4,14 @@ import com.manjesh.rest.model.Event;
 
 import java.time.OffsetDateTime;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import javax.ejb.Singleton;
 import javax.ejb.Stateless;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -21,9 +26,38 @@ public class EventService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Event> GetEvents() {
-        return Arrays.asList(
-                new Event("Device-1", "Network-1", OffsetDateTime.now()),
-                new Event("Device-2", "Network-2", OffsetDateTime.now().plusDays(2))
-        );
+        return DataSource.GetEvents();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("add")
+    public List<Event> AddEvent(Event newEvent) {
+        return DataSource.AddEvent(newEvent);
+    }
+
+    static class DataSource {
+        static List<Event> events;
+
+        static {
+            events = new LinkedList<>();
+            events.add(new Event("Device-1", "Network-1", OffsetDateTime.now()));
+            events.add(new Event("Device-2", "Network-2", OffsetDateTime.now().plusDays(2)));
+        }
+
+        static public List<Event> AddEvent(Event event) {
+            System.out.println(" ******* Adding event now ********");
+            if (null == event)
+                System.out.println(" ******* NULL NULL NULL NULL ********");
+            else
+                System.out.println(event.getDeviceID());
+
+            events.add(event);
+            return events;
+        }
+
+        static public List<Event> GetEvents() {
+            return events;
+        }
     }
 }
